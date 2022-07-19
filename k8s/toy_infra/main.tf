5a06f104906da0ad3ab95191d2ee9485e398c9d6
jslsn <jordan.s.layne@gmail.com>
2022-07-19 20:54:51 +0100

    Rejigged and added a pulic cluster for ease :)

--- k8s/toy_infra/main.tf
+++ k8s/toy_infra/main.tf
@@ -22,14 +22,6 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-resource "aws_nat_gateway" "nat_gateway" {
-  depends_on = [aws_subnet.public_subnets]
-  subnet_id  = aws_subnet.public_subnets["public_subnet_1"].id
-  tags = {
-    Name = "jl_k8s_igw"
-  }
-}
-
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
@@ -42,18 +34,6 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name      = "private_route_table"
-    Terraform = true
-  }
-}
-
 resource "aws_route_table_association" "public" {
   depends_on     = [aws_subnet.public_subnets]
   route_table_id = aws_route_table.public_route_table.id
@@ -61,12 +41,6 @@ resource "aws_route_table_association" "public" {
   subnet_id      = each.value.id
 }
 
-resource "aws_route_table_association" "private" {
-  depends_on     = [aws_subnet.private_subnets]
-  route_table_id = aws_route_table.private_route_table.id
-  for_each       = aws_subnet.private_subnets
-  subnet_id      = each.value.id
-}
 
 resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
@@ -80,14 +54,37 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
-resource "aws_subnet" "private_subnets" {
-  for_each          = var.private_subnets
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
-  tags = {
-    Name      = each.key
-    Terrafrom = true
-  }
+resource "aws_iam_role" "eks_toy_role" {
+  name = "public_eks_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+    }]
+  })
+
 }
 
+resource "aws_iam_role_policy_attachment" "EKS-ClusterPerms" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_toy_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EKS-VPCPerms" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_toy_role.name
+}
+
+resource "aws_eks_cluster" "jl_toy_cluster" {
+  name     = "public_eks_toy"
+  role_arn = aws_iam_role.eks_toy_role.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.public_subnets["public_subnet_1"].id,aws_subnet.public_subnets["public_subnet_2"].id]
+  }
+}
--- k8s/toy_infra/variables.tf
+++ k8s/toy_infra/variables.tf
@@ -17,10 +17,3 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
-variable "private_subnets" {
-  default = {
-    "private_subnet_1" = 1
-    "private_subnet_2" = 2
-    "private_subnet_3" = 3
-  }
-}
